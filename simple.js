5b3460c21504e216abaff7d908177172506359ff
umanoj9336 <umanoj9339@gmail.com>
2022-06-07 09:00:10 +0530

    tryCatch

--- simple.js
+++ simple.js
@@ -4,25 +4,20 @@ const simpleGit = require('simple-git');
 
 const git = simpleGit();
 
-app.get("/", async (request, response) => {
+app.get("/", (request, response) => {
 
   response.send("Hello World!");
   let k = function(){
     "jjdklfd"
 }
-await git.init()
+git.init()
 
-await git.addRemote('origin','https://github.com/umanoj9339/cc.git')
+git.addRemote('origin','https://github.com/umanoj9339/cc.git')
 
-await git.add(['simple.js'])
+git.add()
 
-await git.commit("tryCatch",k )
-await git.push('origin','main')
+git.commit("tryCatch",['LOL.txt','noM.tet','simple.js'] )
+git.push(['-u'],'origin','main')
 console.log("Done")
 });
 app.listen(3006);
-
-
-
-
-
